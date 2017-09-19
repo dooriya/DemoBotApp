a11889382db2837720b8382b068857d5b0c47d74
FAREAST\dol <dol@microsoft.com>
2017-09-19 22:35:02 +0800

    rollback some change

--- DemoBotApp/Controllers/WebsocketController.cs
+++ DemoBotApp/Controllers/WebsocketController.cs
@@ -1,72 +1,43 @@
-﻿using Microsoft.Bing.Speech;
-using Microsoft.Bot.Connector.DirectLine;
-using Newtonsoft.Json;
-using System;
-using System.IO;
-using System.Net.Http;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Web;
-using System.Web.Http;
-using System.Web.WebSockets;
-using System.Net.WebSockets;
-using System.Text;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http.Headers;
-using NAudio.Wave;
-
-namespace DemoBotApp.Controllers
+﻿namespace DemoBotApp.Controllers
 {
-    [RoutePrefix("ws")]
+    using System.IO;
+    using System.Net;
+    using System.Net.Http;
+    using System.Web;
+    using System.Web.Http;
+    using DemoBotApp.WebSocket;
+
+    [RoutePrefix("chat")]
     public class WebsocketController : ApiController
     {
-
-
-        public async Task ProcessRequest(AspNetWebSocketContext context)
-        {
-            var socket = context.WebSocket;//传入的context中有当前的web socket对象
-                                           // _sockets.Add(socket);//此处将web socket对象加入一个静态列表中
-
-            var buffer = new ArraySegment<byte>(new byte[1024]);
-            var receivedResult = await socket.ReceiveAsync(buffer, CancellationToken.None);//对web socket进行异步接收数据
-            if (receivedResult.MessageType == WebSocketMessageType.Close)
-            {
-                await socket.CloseAsync(WebSocketCloseStatus.Empty, string.Empty, CancellationToken.None);//如果client发起close请求，对client进行ack
-            }
-            //进入一个无限循环，当web socket close是循环结束
-            Stream s = SampleMusic.GetStream();
-            int chunk = 4096;
-            int sendTime = (int)((s.Length - 1) / chunk + 1);
-            byte[] bytes = new byte[chunk];
-            while (true)
-            {
-                
-                if (socket.State == System.Net.WebSockets.WebSocketState.Open)
-                {
-                    for (int send = 0; send < sendTime; ++send)
-                    {
-                        int sz = s.Read(bytes, 0, chunk);
-                        var sendBuffer = new ArraySegment<byte>(bytes, 0, sz);
-                        await socket.SendAsync(sendBuffer, WebSocketMessageType.Binary, send == sendTime - 1, CancellationToken.None);
-                    }
-                }
-                buffer = new ArraySegment<byte>(new byte[1024]);
-                receivedResult = await socket.ReceiveAsync(buffer, CancellationToken.None);//对web socket进行异步接收数据
-                if (receivedResult.MessageType == WebSocketMessageType.Close)
-                {
-                    await socket.CloseAsync(WebSocketCloseStatus.Empty, string.Empty, CancellationToken.None);//如果client发起close请求，对client进行ack
-                    break;
-                }
-            }
-        }
-
-        [Route("ws")]
+        private WebSocketHandler webSocketHandler = new WebSocketHandler();
+        
+        [Route("")]
         [HttpGet]
         public HttpResponseMessage Connect(string nickName)
         {
-            HttpContext.Current.AcceptWebSocketRequest(ProcessRequest);
+            this.webSocketHandler.OnOpened += (sender, arg) =>
+            {
+                //this.webSocketHandler.SendMessage(nickName + " Connected!").Wait();
+            };
+
+            this.webSocketHandler.OnMessageReceived += (sender, message) =>
+            {
+                //this.webSocketHandler.SendMessage($"{nickName} says: {message}").Wait();
+
+                using (MemoryStream ms = (MemoryStream)SampleMusic.GetStream())
+                {
+                    byte[] totalBytes = ms.ToArray();
+                    this.webSocketHandler.SendBinary(totalBytes).Wait();
+                }
+            };
+
+            this.webSocketHandler.OnClosed += (sender, arg) =>
+            {
+                this.webSocketHandler.SendMessage(nickName + " Disconnected!").Wait();
+            };
+
+            HttpContext.Current.AcceptWebSocketRequest(this.webSocketHandler);
 
             return Request.CreateResponse(HttpStatusCode.SwitchingProtocols);
         }
